b8374ed8a224859317420251e9b812101421eb00
Anthony Greb <agreb255@gmail.com>
2019-05-15 16:31:34 -0400

    added footer

---
+++ portfolio/src/components/footerSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {FaRegCopyright} from 'react-icons/fa';
+
+export default function FooterSection(){
+return(
+    <footer>
+        <div className="footerFlex">
+        <p><FaRegCopyright/>Anthony Greb...If you have quesions about the site send me a message above.</p>
+        </div> 
+    </footer>
+)
+}
